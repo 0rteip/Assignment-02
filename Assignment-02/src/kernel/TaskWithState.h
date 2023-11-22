52f995148afd1c7d75de1851ed44b777a76aef55
Pietro Ventrucci <pietro.ventrucci@studio.unibo.it>
2023-11-22 13:12:31 +0100

    Used smart_bridge as template

---
+++ Assignment-02/src/kernel/TaskWithState.h
@@ -0,0 +1,28 @@
+#ifndef __TASK_WITH_STATE__
+#define __TASK_WITH_STATE__
+
+#include "kernel/Task.h"
+#include "Arduino.h"
+
+class TaskWithState: public Task {
+
+public:
+  TaskWithState() 
+    : Task(){
+  }
+
+  void setState(int s){
+      state = s;
+      stateTimestamp = millis();
+  }
+
+  long elapsedTimeInState(){
+      return millis() - stateTimestamp;
+  }
+  
+private:
+  int state;
+  long stateTimestamp;
+};
+
+#endif
--- Assignment-02/src/main.cpp
+++ Assignment-02/src/main.cpp
@@ -1,18 +1,64 @@
+/*
+ * SMART ROOM - Assignment #02 - ESIOT a.y. 2022-2023
+ * 
+ * Solution sketch by AR
+ * 
+ */
 #include <Arduino.h>
+#include "config.h"
+#include "kernel/Scheduler.h"
+#include "kernel/Logger.h"
+#include "kernel/MsgService.h"
+#include "UserConsole.h"
 
-// put function declarations here:
-int myFunction(int, int);
+#include "model/Bridge.h"
+#include "tasks/LightControlTask.h"
+#include "tasks/WaterControlTask.h"
+#include "tasks/UserInputControlTask.h"
+#include "tasks/BlinkTask.h"
+#include "tasks/ValveControlTask.h"
+
+Scheduler sched;
+
+Bridge* pBridge;
+UserConsole* pUserConsole;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  MsgService.init();
+  sched.init(50);
+
+  Logger.log(".:: Smart Bridge  ::.");
+  
+  pBridge = new Bridge();
+  pBridge->init();
+
+  pUserConsole = new UserConsole();
+  pUserConsole->init();
+
+  Task* pLightControlTask = new LightControlTask(pBridge);
+  pLightControlTask->init(100);
+
+  Task* pBlinkTask = new BlinkTask(LEDC_PIN);
+  pBlinkTask->init(2000);
+  pBlinkTask->setActive(false);
+
+  Task* pValveControlTask = new ValveControlTask(pBridge, pUserConsole);
+  pValveControlTask->init(200);
+
+  Task* pUserInputControlTask = new UserInputControlTask(pBridge, pUserConsole);
+  pUserInputControlTask->init(50);
+
+  Task* pWaterControlTask = new WaterControlTask(pBridge, pBlinkTask, pValveControlTask);
+  pWaterControlTask->init(100);
+
+  sched.addTask(pLightControlTask);
+  sched.addTask(pUserInputControlTask);  
+  sched.addTask(pWaterControlTask);  
+  sched.addTask(pValveControlTask);  
+  sched.addTask(pBlinkTask);  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    sched.schedule();
+    // pUserConsole->test();
 }
