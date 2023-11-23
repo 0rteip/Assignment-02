4fcbda906651f4e0bd0c0b4d30cb5c7c09a826f2
Luca Carabini <luca02c2@gmail.com>
2023-11-23 16:50:05 +0100

    Add display, led features

--- Assignment-02/src/model/CarWash.cpp
+++ Assignment-02/src/model/CarWash.cpp
@@ -2,13 +2,47 @@
 #include "config.h"
 #include "devices/Led.h"
 #include "kernel/Logger.h"
-
+#include "CarWash.h"
+#include <Arduino.h>
 #define MAX_DISTANCE_FROM_RIVER 3.00
 
 CarWash::CarWash(){
 }
 
 void CarWash::init(){
-
+    pir = new Pir(PIR_PIN);
+    leds[0] = new Led(LED1_PIN);
+    leds[1] = new Led(LED2_PIN);
+    leds[2] = new Led(LED3_PIN);
+    lcd = new DisplayLcdI2C();
+    //gate = new ServoMotor();
 }
 
+bool CarWash::getPrecence() {
+    this->pir->sync();
+    return this->pir->isDetected();
+}
+
+void CarWash::switchL1() {
+    if (this->leds[0]->isOn()) {
+        //this->leds[0]->switchOff();
+    } else {
+        // this->leds[0]->switchOn();
+        Serial.println("L1 Acceso");
+    }
+}
+
+void CarWash::displayMessage(char string[]) {
+    lcd->clear();
+    lcd->display(string);
+}
+
+void CarWash::setCarInState() {
+    this->displayMessage("Proceed to the Washing Area");
+    //this->gate->setPosition
+}
+
+void CarWash::scrollDisplay()
+{
+    this->lcd->scroll();
+}
--- Assignment-02/src/model/CarWash.h
+++ Assignment-02/src/model/CarWash.h
@@ -5,14 +5,27 @@
 #include "devices/Led.h"
 #include "devices/Pir.h"
 #include "devices/Sonar.h"
-
+#include "devices/DisplayLcdI2C.h"
+#include "devices/servo_motor.h"
+#include "config.h"
 class CarWash {
     
 public: 
     CarWash();
     void init();
+    bool getPrecence();
+    void switchL1();
+    void displayMessage(char string[]);
+    void setCarInState();
+    void scrollDisplay();
     
-   
+private:
+    bool detect;
+    bool stateL1;
+    Pir* pir;
+    Led* leds[LED_NUM];
+    DisplayLcdI2C* lcd;
+    ServoMotor* gate;
 };
 
 
