e2f51fb5f55aaa1be9d0d0ef3436fc6fe35a0ddc
Luca Carabini <luca02c2@gmail.com>
2023-11-23 16:48:38 +0100

    Add new Task

--- Assignment-02/src/main.cpp
+++ Assignment-02/src/main.cpp
@@ -10,23 +10,29 @@
 #include "kernel/Logger.h"
 #include "kernel/MsgService.h"
 #include "model/CarWash.h"
+#include "tasks/DetectTask.h"
+#include "tasks/BlinkLedTask.h"
 
 Scheduler sched;
-
 CarWash* carWash;
 
 void setup() {
-  MsgService.init();
-  sched.init(50);
-
-  Logger.log(".:: Smart Bridge  ::.");
-  
-  carWash = new CarWash();
-  carWash->init();
-  
+    MsgService.init();
+    sched.init(50);
+    Serial.begin(9600);
+    Logger.log(".:: Smart Bridge  ::."); 
+    carWash = new CarWash();
+    carWash->init();
+	Task* bilnkLedTask = new BlinkLedTask(LED1_PIN);
+    bilnkLedTask->init(100);
+	bilnkLedTask->setActive(false);
+    Task* detectTask = new DetectTask(carWash, bilnkLedTask);
+    detectTask->init(1000);
+    sched.addTask(detectTask);
+	sched.addTask(bilnkLedTask);
 }
 
 void loop() {
     sched.schedule();
-    // pUserConsole->test();
+    //pUserConsole->test();
 }
