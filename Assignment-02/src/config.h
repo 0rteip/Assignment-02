3812f4eef8a639e352b3eebcbe901da1b3dcd65a
Luca Carabini <luca02c2@gmail.com>
2023-11-23 16:51:44 +0100

    No relavant mod

--- Assignment-02/src/config.h
+++ Assignment-02/src/config.h
@@ -1,11 +1,13 @@
 #ifndef __CONFIG__
 #define __CONFIG__
 
-#define LED1_PIN 5
+#define LED1_PIN 13
 #define BT_PIN 3
 
 #define LED2_PIN 6
 #define LED3_PIN 4
+
+#define LED_NUM 3
 
 #define PIR_PIN 2
 
@@ -18,5 +20,4 @@
 #define N2 2.00
 #define N3 3.00
 #define N4 3.00
-
 #endif
