a8c1338cc3d5a0bed11af7613d1b8208888fc645
Luca Carabini <luca02c2@gmail.com>
2023-11-23 10:15:08 +0100

    Add kernel dir, and config file

---
+++ Assignment-02/src/config.h
@@ -0,0 +1,22 @@
+#ifndef __CONFIG__
+#define __CONFIG__
+
+#define LED1_PIN 5
+#define BT_PIN 3
+
+#define LED2_PIN 6
+#define LED3_PIN 4
+
+#define PIR_PIN 2
+
+#define SONAR_TRIG_PIN 7
+#define SONAR_ECHO_PIN 8
+
+#define GATE_PIN 9
+
+#define N1 1.00
+#define N2 2.00
+#define N3 3.00
+#define N4 3.00
+
+#endif
