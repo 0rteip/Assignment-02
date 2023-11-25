2213a684ab3aa09ad35563376e822d2f8051f94d
Pietro Ventrucci <pietro.ventrucci@studio.unibo.it>
2023-11-25 11:20:35 +0100

    Proximity abstract class

--- Assignment-02/src/devices/ProximitySensor.h
+++ Assignment-02/src/devices/ProximitySensor.h
@@ -1,13 +1,11 @@
-#ifndef __PROXIMITYSENSOR__
-#define __PROXIMITYSENSOR__
+#ifndef __PROX_SENSOR__
+#define __PROX_SENSOR__
 
-class ProximitySensor {
+class ProximitySensor
+{
 
 public:
   virtual float getDistance() = 0;
-  
 };
 
-
 #endif
-
