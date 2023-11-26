f442229f8f06b2842b315a24af35df7a459962c5
Pietro Ventrucci <pietro.ventrucci@studio.unibo.it>
2023-11-26 22:48:52 +0100

    Add car wash states and distance feature

--- Assignment-02/src/model/CarWash.h
+++ Assignment-02/src/model/CarWash.h
@@ -4,31 +4,42 @@
 #include <Arduino.h>
 #include "devices/Led.h"
 #include "devices/Pir.h"
-#include "devices/ProximitySensor.h"
+#include "devices/UltrasonicSensor.h"
+#include "devices/TempSensor.h"
 #include "devices/DisplayLcdI2C.h"
 #include "devices/servo_motor.h"
 #include "config.h"
-class CarWash {
-    
-public: 
+
+#define GATE_CLOSE 0
+#define GATE_OPEN 180
+
+class CarWash
+{
+public:
     CarWash();
     void init();
     bool getPrecence();
     void switchL1();
     void displayMessage(char string[]);
     void setCarInState();
+    void setFullyEnteredState();
+    void setWashingState();
+    void setWashingCompletedState();
+    void setCarOutState();
+    bool isWashingStarted();
     void scrollDisplay();
     unsigned long getCarDistance();
-    
+
 private:
     bool detect;
     bool stateL1;
-    Pir* pir;
-    Led* leds[LED_NUM];
-    DisplayLcdI2C* lcd;
-    ServoMotor* gate;
-    ProximitySensor* prox;
+    float washingAreaTemperture;
+    Pir *pir;
+    Led *leds[LED_NUM];
+    DisplayLcdI2C *lcd;
+    ServoMotor *gate;
+    UltrasonicSensor *prox;
+    TempSensor *tempSensor;
 };
-
 
 #endif
