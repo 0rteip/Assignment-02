bac5a6522a818e9988e9fce70df00a90cd8c3770
Pietro Ventrucci <pietro.ventrucci@studio.unibo.it>
2023-11-28 00:38:45 +0100

    Rearrange

--- Assignment-02/src/main.cpp
+++ Assignment-02/src/main.cpp
@@ -1,8 +1,8 @@
 /*
  * SMART ROOM - Assignment #02 - ESIOT a.y. 2022-2023
- * 
+ *
  * Solution sketch by AR
- * 
+ *
  */
 #include <Arduino.h>
 #include "config.h"
@@ -14,36 +14,36 @@
 #include "tasks/BlinkLedTask.h"
 #include "tasks/ControlTask.h"
 #include "tasks/DistanceControlTask.h"
-#include "devices/servo_motor.h"
-#include "devices/servo_motor_impl.h"
 #include "kernel/TaskWithReinit.h"
 #include "UserConsole.h"
+
 Scheduler sched;
-CarWash* carWash;
-ServoMotor* motor;
-UserConsole* userConsole;
-void setup() {
-   //MsgService.init();
-   Serial.begin(9600);
+CarWash *carWash;
+UserConsole *userConsole;
+void setup()
+{
+    // MsgService.init();
+    Serial.begin(9600);
     sched.init(50);
-    carWash = new CarWash();
     userConsole = new UserConsole();
+
+    carWash = new CarWash(userConsole);
     carWash->init();
-	Task* blinkLedTask = new BlinkLedTask(LED2_PIN);
+    Task *blinkLedTask = new BlinkLedTask(LED2_PIN);
     blinkLedTask->init(100);
-	blinkLedTask->setActive(false);
-    Task* butTask = new ControlTask(blinkLedTask, userConsole, carWash);
+    blinkLedTask->setActive(false);
+    Task *butTask = new ControlTask(blinkLedTask, userConsole, carWash);
     butTask->init(200);
-    Task* detectTask = new DetectTask(carWash, blinkLedTask);
+    Task *detectTask = new DetectTask(carWash, blinkLedTask);
     detectTask->init(1000);
     sched.addTask(detectTask);
-	sched.addTask(blinkLedTask);
+    sched.addTask(blinkLedTask);
     sched.addTask(butTask);
 }
 
+void loop()
+{
 
-void loop() {
-   
     sched.schedule();
-    //pUserConsole->test();
+    // pUserConsole->test();
 }
