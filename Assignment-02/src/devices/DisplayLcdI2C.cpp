7e28a47ca08f5d15df4667a3ca56ce5eee07d66c
Pietro Ventrucci <pietro.ventrucci@studio.unibo.it>
2023-11-27 23:31:48 +0100

    Add clear to display message

--- Assignment-02/src/devices/DisplayLcdI2C.cpp
+++ Assignment-02/src/devices/DisplayLcdI2C.cpp
@@ -2,18 +2,22 @@
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
 
-DisplayLcdI2C::DisplayLcdI2C() {
-    this->lcd = new LiquidCrystal_I2C(0x27,16,2); 
+DisplayLcdI2C::DisplayLcdI2C()
+{
+    this->lcd = new LiquidCrystal_I2C(0x27, 16, 2);
     this->lcd->init();
     this->lcd->backlight();
     this->lcd->leftToRight();
 }
 
-void DisplayLcdI2C::display(const char string[]) {
+void DisplayLcdI2C::display(const char string[])
+{
+    this->clear();
     lcd->print(string);
 }
 
-void DisplayLcdI2C::clear() {
+void DisplayLcdI2C::clear()
+{
     this->lcd->clear();
 }
 
