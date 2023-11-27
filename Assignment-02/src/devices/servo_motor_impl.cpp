6213e49ae6e29cad641a0fb3e1692f2566cd776e
Pietro Ventrucci <pietro.ventrucci@studio.unibo.it>
2023-11-27 12:23:52 +0100

    Status Message

--- Assignment-02/src/devices/servo_motor_impl.cpp
+++ Assignment-02/src/devices/servo_motor_impl.cpp
@@ -1,28 +1,42 @@
 #include "servo_motor_impl.h"
 #include "Arduino.h"
 
-ServoMotorImpl::ServoMotorImpl(int pin){
-  this->pin = pin;  
-} 
-
-void ServoMotorImpl::on(){
-  // updated values: min is 544, max 2400 (see ServoTimer2 doc)
-  motor.attach(pin); //, 544, 2400);    
+ServoMotorImpl::ServoMotorImpl(int pin)
+{
+  this->pin = pin;
 }
 
-void ServoMotorImpl::setPosition(int angle){
-	if (angle > 180){
-		angle = 180;
-	} else if (angle < 0){
-		angle = 0;
-	}
-  // 750 -> 0, 2250 -> 180 
+void ServoMotorImpl::on()
+{
+  // updated values: min is 544, max 2400 (see ServoTimer2 doc)
+  motor.attach(pin); //, 544, 2400);
+  Serial.println("Motor on");
+}
+
+void ServoMotorImpl::setPosition(int angle)
+{
+  if (angle > 180)
+  {
+    angle = 180;
+  }
+  else if (angle < 0)
+  {
+    angle = 0;
+  }
+  // 750 -> 0, 2250 -> 180
   // 750 + angle*(2250-750)/180
   // updated values: min is 544, max 2400 (see ServoTimer2 doc)
-  float coeff = (2400.0-544.0)/180;
-  motor.write(544 + angle*coeff);              
+  float coeff = (2400.0 - 544.0) / 180;
+  motor.write(544 + angle * coeff);
+
+  Serial.print("Motor position: ");
+  Serial.println(angle == 0     ? "CLOSED"
+                 : angle == 180 ? "OPEN"
+                                : String(angle));
 }
 
-void ServoMotorImpl::off(){
-  motor.detach();    
+void ServoMotorImpl::off()
+{
+  Serial.println("Motor off");
+  motor.detach();
 }
