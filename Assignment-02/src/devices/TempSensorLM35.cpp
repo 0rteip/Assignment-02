7bbb300a98ec83d5554ef70254302ec816d8a42d
Pietro Ventrucci <pietro.ventrucci@studio.unibo.it>
2023-11-28 00:27:08 +0100

    Create LM35 temp sensor variant

--- Assignment-02/src/devices/TempSensorLM35.cpp
+++ Assignment-02/src/devices/TempSensorLM35.cpp
@@ -1,14 +1,29 @@
 #include "TempSensorLM35.h"
 #include "Arduino.h"
 
-#define VCC ((float)5)
+TempSensorLM35::TempSensorLM35(int pin)
+{
+  // utilizzando l istruzione analogReference
+  // indico al convertitore AD che deve impiegare
+  // la tensione presente sul pin AREF come
+  // valore di riferimento per la conversione
+  analogReference(EXTERNAL);
+  this->pin = pin;
+}
 
-TempSensorLM35::TempSensorLM35(int p) : pin(p){
-} 
-  
-float TempSensorLM35::getTemperature(){
-  int value = analogRead(pin);
-  float valueInVolt = value*VCC/1023;  
-  float valueInCelsius = valueInVolt/0.01;
-  return valueInCelsius;
+float TempSensorLM35::getCelsius()
+{
+  this->valADC = 0;
+
+  for (byte i = 0; i < NUM_SAMPLES; i++)
+  {
+    valADC += analogRead(this->pin);
+    delayMicroseconds(100);
+  }
+  // eseguo la media dei valori letti
+  valADC /= NUM_SAMPLES;
+  // °C = (valoreADC * PrecisioneADC) / stepGradoTensione
+  temp = (valADC * ADC_PRECISION) / 0.01;
+
+  return temp;
 }
--- Assignment-02/src/devices/TempSensorLM35.h
+++ Assignment-02/src/devices/TempSensorLM35.h
@@ -3,13 +3,20 @@
 
 #include "TempSensor.h"
 
-class TempSensorLM35: public TempSensor {
+#define NUM_SAMPLES 8
+// `ADC/1024` (ADC = 3.3V)
+#define ADC_PRECISION 0.0032
+
+class TempSensorLM35 : public TempSensor
+{
 public:
-  TempSensorLM35(int p);
-  virtual float getTemperature();
+  TempSensorLM35(int pin);
+  float getCelsius();
+
 private:
   int pin;
+  int valADC;
+  float temp;
 };
-
 
 #endif
