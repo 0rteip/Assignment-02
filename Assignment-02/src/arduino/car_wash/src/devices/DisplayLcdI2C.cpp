c49ae12519f5f1e736df087c82481cadcdf751b8
Pietro Ventrucci <pietro.ventrucci@studio.unibo.it>
2023-11-30 00:00:03 +0100

    Overcomplicated idea

--- Assignment-02/src/arduino/car_wash/src/devices/DisplayLcdI2C.cpp
+++ Assignment-02/src/arduino/car_wash/src/devices/DisplayLcdI2C.cpp
@@ -8,12 +8,34 @@ DisplayLcdI2C::DisplayLcdI2C()
     this->lcd->init();
     this->lcd->backlight();
     this->lcd->leftToRight();
+
+    this->lcd->createChar(0, zero);
+    this->lcd->createChar(1, one);
+    this->lcd->createChar(2, two);
+    this->lcd->createChar(3, three);
+    this->lcd->createChar(4, four);
+    this->lcd->createChar(5, five);
 }
 
 void DisplayLcdI2C::display(String string)
 {
+    Serial.println(string);
     this->clear();
     lcd->print(string);
+}
+
+void DisplayLcdI2C::updateProgress(int progress)
+{
+    this->lcd->setCursor(0, 0);
+    this->lcd->print("Progress: ");
+    this->lcd->print(progress);
+    this->lcd->print("%");
+
+    Serial.print("Progress: ");
+    Serial.print(progress);
+    Serial.println("%");
+
+    this->updateProgressBar(progress, 100, 1);
 }
 
 void DisplayLcdI2C::clear()
@@ -34,4 +56,60 @@ void DisplayLcdI2C::off()
 void DisplayLcdI2C::on()
 {
     this->lcd->backlight();
+}
+
+void DisplayLcdI2C::updateProgressBar(unsigned long progress, unsigned long totalCount, int lineToPrintOn)
+{
+    double factor = totalCount / 80.0;
+    int percent = (progress + 1) / factor;
+    int number = percent / 5;
+    int remainder = percent % 5;
+
+    if (number > 0)
+    {
+        // stampo il primo totalmente pieno (p1)
+        Serial.print("[f");
+
+        // stampo i successivi che sono pieni (p2)
+        for (int j = 1; j < number; j++)
+        {
+            this->lcd->setCursor(j, lineToPrintOn);
+            this->lcd->write(5);
+            Serial.print(5);
+        }
+        // Stampo rimanente, 
+        if (remainder != 0)
+        {
+            Serial.print(remainder);
+            number++;
+        }
+    }
+    else
+    {
+        Serial.print("[");
+        Serial.print(remainder);
+        number++;
+    }
+
+    this->lcd->setCursor(number, lineToPrintOn);
+    this->lcd->write(remainder);
+
+    // Serial.print(" ");
+    // Serial.print(number);
+    // Serial.print(" ");
+
+    // int tmpNumber = number;
+
+    // Stampo spazi vuoti
+    if (number < 16)
+    {
+        for (int j = number + 1; j <= 16; j++)
+        {
+            this->lcd->setCursor(j, lineToPrintOn);
+            this->lcd->write(0);
+            Serial.print(0);
+        }
+    }
+
+    Serial.println("]");
 }
--- Assignment-02/src/arduino/car_wash/src/devices/DisplayLcdI2C.h
+++ Assignment-02/src/arduino/car_wash/src/devices/DisplayLcdI2C.h
@@ -1,16 +1,78 @@
 #ifndef __DISPLAY_LCD_I2C__
 #define __DISPLAY_LCD_I2C__
+
 #include <LiquidCrystal_I2C.h>
-class DisplayLcdI2C { 
+#include <Arduino.h>
+
+class DisplayLcdI2C
+{
 public:
     DisplayLcdI2C();
     void display(String string);
+    void updateProgress(int progress);
     void clear();
     void scroll();
     void off();
     void on();
-private: 
-    LiquidCrystal_I2C* lcd;
+
+private:
+    void updateProgressBar(unsigned long progress, unsigned long totalCount, int lineToPrintOn);
+
+    LiquidCrystal_I2C *lcd;
+    byte zero[8] = {
+        B00000,
+        B00000,
+        B00000,
+        B00000,
+        B00000,
+        B00000,
+        B00000,
+        B00000};
+    byte one[8] = {
+        B10000,
+        B10000,
+        B10000,
+        B10000,
+        B10000,
+        B10000,
+        B10000,
+        B10000};
+    byte two[8] = {
+        B11000,
+        B11000,
+        B11000,
+        B11000,
+        B11000,
+        B11000,
+        B11000,
+        B11000};
+    byte three[8] = {
+        B11100,
+        B11100,
+        B11100,
+        B11100,
+        B11100,
+        B11100,
+        B11100,
+        B11100};
+    byte four[8] = {
+        B11110,
+        B11110,
+        B11110,
+        B11110,
+        B11110,
+        B11110,
+        B11110,
+        B11110};
+    byte five[8] = {
+        B11111,
+        B11111,
+        B11111,
+        B11111,
+        B11111,
+        B11111,
+        B11111,
+        B11111};
 };
 
 #endif
