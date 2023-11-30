107063e69d915d780f3902b93c8e479032387bf9
Pietro Ventrucci <pietro.ventrucci@studio.unibo.it>
2023-11-30 23:16:14 +0100

    Add distanceCOntrolTask to set active state

--- Assignment-02/src/arduino/car_wash/src/tasks/DetectTask.h
+++ Assignment-02/src/arduino/car_wash/src/tasks/DetectTask.h
@@ -5,23 +5,30 @@
 #include "kernel/TaskWithReinit.h"
 #include "model/CarWash.h"
 #include "tasks/BlinkLedTask.h"
-class DetectTask: public Task {
+#include "tasks/DistanceControlTask.h"
+
+class DetectTask : public Task
+{
 
 public:
-  DetectTask(CarWash* carWash, BlinkLedTask* blinkTask); 
+  DetectTask(CarWash *carWash, DistanceControlTask *distanceControlTask, BlinkLedTask *blinkTask);
   void tick();
 
-private:  
-
-  enum State { IDLE, DETECTED, CAR_IN} state;
+private:
+  enum State
+  {
+    IDLE,
+    DETECTED,
+    CAR_IN
+  } state;
   long stateTimestamp;
-
 
   void setState(State state);
   long elapsedTimeInState();
-  CarWash* carWash;
-  BlinkLedTask* blink;
 
+  CarWash *carWash;
+  DistanceControlTask *distanceControlTask;
+  BlinkLedTask *blink;
 };
 
 #endif
