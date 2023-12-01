c3077476979815c1df932565341f2d9fabf16a28
Pietro Ventrucci <pietro.ventrucci@studio.unibo.it>
2023-12-01 20:43:50 +0100

    Clean and reorganize

--- Assignment-02/src/arduino/car_wash/src/main.cpp
+++ Assignment-02/src/arduino/car_wash/src/main.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 #include "config.h"
 #include "kernel/Scheduler.h"
-#include "kernel/Logger.h"
 #include "kernel/MsgService.h"
 #include "model/CarWash.h"
 #include "tasks/DetectTask.h"
@@ -14,18 +13,19 @@
 
 #include "devices/DisplayLcdI2C.h"
 
-Scheduler sched;
+Scheduler *sched;
 CarWash *carWash;
 UserConsole *userConsole;
-DisplayLcdI2C *display;
 
 void setup()
 {
     MsgService.init();
-    sched.init(50);
+    sched = new Scheduler();
+    sched->init(50);
 
     userConsole = new UserConsole();
     carWash = new CarWash(userConsole);
+
     BlinkLedTask *blinkLedTask = new BlinkLedTask(LED2_PIN);
     blinkLedTask->init(100);
     blinkLedTask->setActive(false);
@@ -36,25 +36,20 @@ void setup()
     DistanceControlTask *distanceControlTask = new DistanceControlTask(carWash, blinkLedTask);
     distanceControlTask->init(500);
 
-    Task *detectTask = new DetectTask(carWash,distanceControlTask, blinkLedTask);
+    Task *detectTask = new DetectTask(carWash, distanceControlTask, blinkLedTask);
     detectTask->init(1000);
 
+    Task *tempTask = new TemperatureControlTask(carWash, userConsole);
+    tempTask->init(500);
 
-    sched.addTask(blinkLedTask);
-    sched.addTask(butTask);
-    sched.addTask(detectTask);
-    sched.addTask(distanceControlTask);
-    Task *temp = new TemperatureControlTask(carWash, userConsole);
-    temp->init(200);
-    sched.addTask(temp);
-
-    delay(5000);
+    sched->addTask(blinkLedTask);
+    sched->addTask(butTask);
+    sched->addTask(detectTask);
+    sched->addTask(distanceControlTask);
+    sched->addTask(tempTask);
 }
-
-int n = 0;
 
 void loop()
 {
-    // display->display("Hello World");
-    sched.schedule();
+    sched->schedule();
 }
