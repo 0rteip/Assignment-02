0a0f64b135f535cdae5aa80c4e331a7f2a41adf0
Pietro Ventrucci <pietro.ventrucci@studio.unibo.it>
2023-12-02 18:32:05 +0100

    Format task

--- Assignment-02/src/arduino/car_wash/src/kernel/Task.h
+++ Assignment-02/src/arduino/car_wash/src/kernel/Task.h
@@ -1,23 +1,27 @@
 #ifndef __TASK__
 #define __TASK__
 
-class Task {
+class Task
+{
 
 public:
-  Task(){
+  Task()
+  {
     active = false;
   }
 
   /* periodic */
-  virtual void init(int period){
+  virtual void init(int period)
+  {
     myPeriod = period;
-    periodic = true;  
+    periodic = true;
     active = true;
     timeElapsed = 0;
   }
 
   /* aperiodic */
-  virtual void init(){
+  virtual void init()
+  {
     timeElapsed = 0;
     periodic = false;
     active = true;
@@ -26,44 +30,54 @@ public:
 
   virtual void tick() = 0;
 
-  bool updateAndCheckTime(int basePeriod){
+  bool updateAndCheckTime(int basePeriod)
+  {
     timeElapsed += basePeriod;
-    if (timeElapsed >= myPeriod){
+    if (timeElapsed >= myPeriod)
+    {
       timeElapsed = 0;
       return true;
-    } else {
-      return false; 
+    }
+    else
+    {
+      return false;
     }
   }
 
-  void setCompleted(){
+  void setCompleted()
+  {
     completed = true;
     active = false;
   }
 
-  bool isCompleted(){
+  bool isCompleted()
+  {
     return completed;
   }
 
-  bool isPeriodic(){
+  bool isPeriodic()
+  {
     return periodic;
   }
 
-  bool isActive(){
+  bool isActive()
+  {
     return active;
   }
-  virtual void setActive(bool active){
+  virtual void setActive(bool active)
+  {
     timeElapsed = 0;
     this->active = active;
   }
-protected: 
+
+protected:
   int myPeriod;
+
 private:
   int timeElapsed;
   bool active;
   bool periodic;
   bool completed;
-
 };
 
 #endif
